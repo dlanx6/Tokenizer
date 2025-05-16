eb86bc3fec5ddcde0aa1926a36f0267b578737b1
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2025-05-16 10:30:50 +0800

    modified contract: adding verification function on-chain

--- src/Tokenizer.sol
+++ src/Tokenizer.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -8,6 +8,7 @@ error Tokenizer__NotAuthorized();
 error Tokenizer__TokenAlreadyMinted();
 error Tokenizer__TokenNotMinted();
 error Tokenizer__InvalidHash();
+error Tokenizer__HashAlreadyStored();
 
 /**
  * @title A minter and verifier for PDF with Keccak256 hash
@@ -18,16 +19,19 @@ error Tokenizer__InvalidHash();
 contract Tokenizer is ERC721 {
     /**
      * @dev This event is triggered when a token is minted
-     * @param from represents the owner of the contract
      * @param tokenId represents an ID from off-chain database
      * @param pdfHash represents a keccak256 hash generated from the transcript PDF using ethers.js
      * @param timestamp represents the current date and time when the token is minted
      */
-    event TokenMinted(address indexed from, uint256 indexed tokenId, bytes32 indexed pdfHash, uint256 timestamp);
+    event TokenMinted(uint256 indexed tokenId, bytes32 indexed pdfHash, uint256 timestamp);
 
     /// @dev Stores the tokenId with the related PDF hash
     /// @notice This simply store the tokenId with the hash, used for verification purposes
     mapping(uint256 => bytes32) private s_transcriptHashes;
+
+    /// @dev Stores the fixed-length hash with boolean value
+    /// @notice This determines if the hash is already stored. This prevents duplication when minting.
+    mapping(bytes32 => bool) private s_storedHashes;
 
     /// @dev immutable, can't be modified once initialized
     address private immutable i_owner;
@@ -37,7 +41,7 @@ contract Tokenizer is ERC721 {
 
     /// @notice sets the name and symbol of the NFT
     /// @dev initializes the deployer as the contract owner
-    constructor(address owner) ERC721("DocuNFT", "DNFT") {
+    constructor(address owner) ERC721("TorNFT", "TRNFT") {
         i_owner = owner;
     }
 
@@ -50,20 +54,23 @@ contract Tokenizer is ERC721 {
     /**
      * @dev Mints the tokenId and store the pdfHash with the tokenId as key-value pair
      * @param tokenId represents an ID from off-chain database
-     * @param pdfHash represents a keccak256 hash generated from the transcript PDF using ethers.js
+     * @param pdfHash represents a keccak256 hash generated from the transcript PDF using ethers v6
      */
-    function mintTranscript(uint256 tokenId, bytes32 pdfHash) public onlyAuthorized {
+    function mint(uint256 tokenId, bytes32 pdfHash) public onlyAuthorized {
         if (pdfHash == bytes32(0)) revert Tokenizer__InvalidHash();
+        if (s_storedHashes[pdfHash]) revert Tokenizer__HashAlreadyStored();
         if (_ownerOf(tokenId) != address(0)) revert Tokenizer__TokenAlreadyMinted();
 
         _safeMint(i_owner, tokenId);
+
         s_transcriptHashes[tokenId] = pdfHash;
+        s_storedHashes[pdfHash] = true;
 
         unchecked {
             mintedTokenCount += 1;
         }
 
-        emit TokenMinted(i_owner, tokenId, pdfHash, block.timestamp);
+        emit TokenMinted(tokenId, pdfHash, block.timestamp);
     }
 
     /**
@@ -71,10 +78,23 @@ contract Tokenizer is ERC721 {
      * @param tokenId represents an ID from off-chain database
      * @return Hash of Transcript PDF
      */
-    function getTranscriptHash(uint256 tokenId) public view returns (bytes32) {
+    function getTranscriptHash(uint256 tokenId) public view returns (bytes32)  {
         if (_ownerOf(tokenId) == address(0)) revert Tokenizer__TokenNotMinted();
 
         return s_transcriptHashes[tokenId];
+    }
+
+    /**
+     * @dev verifies the hash of transcript PDF stored on-chain with PDF hash from the frontend
+     * @param tokenId represents an ID from off-chain database
+     * @param pdfHash represents a keccak256 hash generated from the transcript PDF using ethers v6
+     * @return True if the input hash matches the on-chain hash, False if not
+     */
+    function verifyTranscriptHash(uint256 tokenId, bytes32 pdfHash) public view returns (bool) {
+        if (pdfHash == bytes32(0)) revert Tokenizer__InvalidHash();
+        if (_ownerOf(tokenId) == address(0)) revert Tokenizer__TokenNotMinted();
+
+        return pdfHash == s_transcriptHashes[tokenId];
     }
 
     /// @return Number of minted tokens
--- test/TokenizerTest.t.sol
+++ test/TokenizerTest.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
@@ -7,7 +7,8 @@ import {
     Tokenizer__NotAuthorized,
     Tokenizer__TokenAlreadyMinted,
     Tokenizer__TokenNotMinted,
-    Tokenizer__InvalidHash
+    Tokenizer__InvalidHash,
+    Tokenizer__HashAlreadyStored
 } from "../src/Tokenizer.sol";
 import {DeployTokenizer} from "../script/DeployTokenizer.s.sol";
 import {ZkSyncChainChecker} from "foundry-devops/src/ZkSyncChainChecker.sol";
@@ -17,6 +18,7 @@ contract TokenizerTest is Test, ZkSyncChainChecker {
 
     uint256 constant TOKENID = 1234;
     bytes32 constant HASH = 0x7c1d39220cfc66b0a8e2357e1a2ff4143c3e560dbdcaa539f6a6d1e9c6cbaf52;
+    bytes32 constant HASH2 = 0x4d5e90aa248ef893e5db44b9352e2e2ad5a519d6bdb8c9f9b384f5ebad13f720;
     address immutable OWNER = vm.envAddress("OWNER_ADDRESS");
 
     modifier isOwner() {
@@ -36,11 +38,11 @@ contract TokenizerTest is Test, ZkSyncChainChecker {
     function testSenderIsOwner() public {
         vm.expectRevert(Tokenizer__NotAuthorized.selector);
 
-        tokenizer.mintTranscript(TOKENID, HASH);
+        tokenizer.mint(TOKENID, HASH);
     }
 
     function testFirstTimeTokenMinted() public isOwner {
-        tokenizer.mintTranscript(TOKENID, HASH);
+        tokenizer.mint(TOKENID, HASH);
 
         address ownerOfToken = tokenizer.ownerOf(TOKENID);
 
@@ -50,9 +52,19 @@ contract TokenizerTest is Test, ZkSyncChainChecker {
     function testTokenAlreadyMinted() public {
         vm.startPrank(OWNER);
 
-        tokenizer.mintTranscript(TOKENID, HASH);
+        tokenizer.mint(TOKENID, HASH);
         vm.expectRevert(Tokenizer__TokenAlreadyMinted.selector);
-        tokenizer.mintTranscript(TOKENID, HASH);
+        tokenizer.mint(TOKENID, HASH2);
+
+        vm.stopPrank();
+    }
+
+    function testHashAlreadyStored() public {
+        vm.startPrank(OWNER);
+
+        tokenizer.mint(TOKENID, HASH);
+        vm.expectRevert(Tokenizer__HashAlreadyStored.selector);
+        tokenizer.mint(100, HASH);
 
         vm.stopPrank();
     }
@@ -60,11 +72,11 @@ contract TokenizerTest is Test, ZkSyncChainChecker {
     function testInvalidHash() public isOwner {
         vm.expectRevert(Tokenizer__InvalidHash.selector);
 
-        tokenizer.mintTranscript(TOKENID, bytes32(0));
+        tokenizer.mint(TOKENID, bytes32(0));
     }
 
     function testCorrectTokenMintedCount() public isOwner {
-        tokenizer.mintTranscript(TOKENID, HASH);
+        tokenizer.mint(TOKENID, HASH);
 
         assertEq(tokenizer.getTokenMintedCount(), 1);
     }
@@ -76,7 +88,7 @@ contract TokenizerTest is Test, ZkSyncChainChecker {
     }
 
     function testGettingMintedToken() public isOwner {
-        tokenizer.mintTranscript(TOKENID, HASH);
+        tokenizer.mint(TOKENID, HASH);
         bytes32 hashOfToken = tokenizer.getTranscriptHash(1234);
 
         assertEq(hashOfToken, HASH);
